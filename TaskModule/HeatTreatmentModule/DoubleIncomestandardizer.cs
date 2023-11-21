5900833db8b2ba38f9f30e50fd7575a27b01ab2b
Nikita Ustinov <unv@plm-ural.ru>
2023-11-21 15:38:42 +0500

    rewrite exponental standardizier for user input

--- TaskModule/HeatTreatmentModule/DoubleIncomestandardizer.cs
+++ TaskModule/HeatTreatmentModule/DoubleIncomestandardizer.cs
@@ -10,18 +10,40 @@ namespace TaskModule.HeatTreatmentModule
 {
     public static class DoubleIncomestandardizer
     {
-        public static string ExponentConvertion(string income, int numbersAfterPoint)
+        public static string ExponentConvertion(string income, int numbersAfterPoint = 6)
         {
-            var pattern = @"^(|-)(\d{1})([.,])\d{0, " + numbersAfterPoint + @"}([Ee])([+-])(\d+)";
-            if (Regex.IsMatch(income, pattern))
-                return income.Replace(',', '.').Replace('e', 'E');
+            income = income.Trim();
 
             if (float.TryParse(income, out float number))
             {
-                return number.ToString("E", CultureInfo.CreateSpecificCulture("en-US"));
+                return number.ToString($"E{numbersAfterPoint}", CultureInfo.CreateSpecificCulture("en-US"));
             }
+
+            if (income.Contains("^") && income.Contains("*"))
+                return TranslateUnCommonUserInput(income);
+
             else
                 throw new ArgumentException("Введенную строку нельзя перевести в float");
+        }
+
+        private static string TranslateUnCommonUserInput(string income)
+        {
+            var rawParts = income.Split('*');
+            var expParts = rawParts[1].Split('^');
+
+            var powIndex = HandlePowerIndexPart(expParts[1].Trim('(', ')'));
+            return $"{rawParts[0]}E{powIndex}";
+        }
+
+        private static string HandlePowerIndexPart(string powerIndex)
+        {
+            if (powerIndex.Contains('/'))
+            {
+                var fraction = powerIndex.Split('/');
+                return (double.Parse(fraction[0]) / double.Parse(fraction[1])).ToString();
+            }
+
+            return double.Parse(powerIndex).ToString();
         }
     }
 }
