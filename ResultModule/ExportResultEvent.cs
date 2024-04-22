536796e75275b6ac0187d8ee04dff949ef75e1a2
Nikita Ustinov <unv@plm-ural.ru>
2024-04-22 16:45:58 +0500

    Added event to linc export method with result page

---
+++ ResultModule/ExportResultEvent.cs
@@ -0,0 +1,23 @@
+﻿using ModelInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultModule
+{
+    public class ExportResultEvent : EventArgs
+    {
+        public float Time { get; set; }
+        public ObjType ObjType { get; set; }
+        public string ResName { get; set; }
+
+        public ExportResultEvent(float time, ObjType objType, string resName)
+        {
+            Time = time;
+            ObjType = objType;
+            ResName = resName;
+        }
+    }
+}
