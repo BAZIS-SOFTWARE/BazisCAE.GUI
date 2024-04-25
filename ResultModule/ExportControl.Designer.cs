801b2c84e4e4697cfedfc1c25dcb361563b51f2e
GreatSherhebe1 <sherhebe1@mail.ru>
2024-04-25 11:34:03 +0500

    Added painting selected line in exportControl

--- ResultModule/ExportControl.Designer.cs
+++ ResultModule/ExportControl.Designer.cs
@@ -43,14 +43,14 @@
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.richTextBox1, 2);
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(150, 53);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(5, 0, 7, 0);
+            this.richTextBox1.Location = new System.Drawing.Point(152, 53);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(301, 220);
+            this.richTextBox1.Size = new System.Drawing.Size(299, 220);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.richTextBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseClick);
             // 
             // btnExport
             // 
@@ -111,7 +111,6 @@
             this.cmbNodeGroupName.Name = "cmbNodeGroupName";
             this.cmbNodeGroupName.Size = new System.Drawing.Size(299, 21);
             this.cmbNodeGroupName.TabIndex = 5;
-            this.cmbNodeGroupName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             // 
             // label3
             // 
@@ -154,13 +153,12 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ExportControl";
             this.Size = new System.Drawing.Size(458, 360);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
