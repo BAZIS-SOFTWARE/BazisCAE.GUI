2fddff76800ccec8438b3fc317512d8aa8e812c8
Nikita Ustinov <unv@plm-ural.ru>
2024-06-04 12:42:10 +0500

    Feature: added text-field-validator

---
+++ TaskModule/Validation/TextFieldsValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskModule.Validation
+{
+    public class TextFieldsValidator : ValidationAttribute
+    {
+        private readonly char[] IligalSymbols = new[] {' '};
+
+        public override bool IsValid(object value)
+        {
+            if (value is string str)
+            {
+                if (str != null && !str.All(x => IligalSymbols.Contains(x)))
+                    return true;
+            }
+            ErrorMessage = "Переданная строка пуста или содержит неподдерживаемые символы.";
+            return false;
+        }
+    }
+}
