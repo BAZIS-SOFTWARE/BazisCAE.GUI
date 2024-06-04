e8199ad4306c7499065750155b2b4cb9517c6fb3
Nikita Ustinov <unv@plm-ural.ru>
2024-06-04 12:18:44 +0500

    Feature: added validator for functions fields

---
+++ TaskModule/Validation/FunctionValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskModule.Validation
+{
+    public class FunctionValidator : ValidationAttribute
+    {
+        public List<string> AvailableFunctions { get; set; }
+
+        public FunctionValidator(List<string> functions) 
+        {
+            AvailableFunctions = functions;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is string str)
+            {
+                if (AvailableFunctions.Contains(str) || float.TryParse(str, out float e))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
