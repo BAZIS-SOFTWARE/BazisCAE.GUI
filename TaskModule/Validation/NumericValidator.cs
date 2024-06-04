ff7d2c67acd3a2498e5135f1a88923623f81a5dd
Nikita Ustinov <unv@plm-ural.ru>
2024-06-04 11:55:20 +0500

    Feature: added validator for numeric input types

---
+++ TaskModule/Validation/NumericValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TaskModule.Validation
+{
+    public class NumericValidator : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is string str)
+            {
+                if (str == null || str.Equals(string.Empty))
+                {
+                    ErrorMessage = "Поле не заполнено";
+                    return false;
+                }
+                    
+
+                if (Regex.IsMatch(str, "^([-]?)(([1-9]{1,})|([0-9]{1}))([.](\\d{1,}))?$")
+                    || Regex.IsMatch(str, "^([-]?)([0-9]{1})(([.])([0-9]{1,}))?([e])([+]|[-])([0]|[1-9]{1,})$"))
+                    return true;
+
+            }
+            ErrorMessage = "Введенное значение строки не соответствует записи числа в обычном или экспоненциальном виде";
+            return false;
+        }
+    }
+}
