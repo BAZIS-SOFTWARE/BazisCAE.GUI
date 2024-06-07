6f545c2f111e000b6af9dab2f2043ea3da660180
Nikita Ustinov <unv@plm-ural.ru>
2024-06-07 17:36:44 +0500

    Update: made method and eventArgs for validation

--- TaskModule/Validation/FunctionValidator.cs
+++ TaskModule/Validation/FunctionValidator.cs
@@ -1,30 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TaskModule.Validation
 {
-    public class FunctionValidator : ValidationAttribute
+    public static class FunctionValidator
     {
-        public List<string> AvailableFunctions { get; set; }
-
-        public FunctionValidator(List<string> functions) 
+        public static bool IsFunctionValueValid(ComboBox cmb, out string errorMessage)
         {
-            AvailableFunctions = functions;
+            var functions = cmb.Items;
+            var value = cmb.Text;
+
+            if (!float.TryParse(value, out float temp))
+            {
+                if (functions.Contains(value))
+                {
+                    errorMessage = string.Empty;
+                    return true;
+                }
+                errorMessage = "Переданное значение не является числом и не содержится в наборе доступных функций." +
+                    "Вероятно, допущена ошибка при выборе функции или при вводе числа";
+                return false;
+            }
+
+            errorMessage = string.Empty;
+            return true;
         }
 
-        public override bool IsValid(object value)
+        public static void cmb_Validating(object sender, ValidationEventArgs e)
         {
-            if (value is string str)
-            {
-                if (AvailableFunctions.Contains(str) || float.TryParse(str, out float e))
-                    return true;
-            }
-            ErrorMessage = "Ввыбранная функция была изменена, или числовое значение задано с ошибкой";
-            return false;
+            string errorMessage = string.Empty;
+            if (!IsFunctionValueValid(e.component as ComboBox, out errorMessage))
+                e.Cancel = true;
+
+            e.EP.SetError(e.component as ComboBox, errorMessage);
         }
     }
 }
