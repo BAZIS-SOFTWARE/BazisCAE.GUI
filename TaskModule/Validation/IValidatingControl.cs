b258ccda225e6ded4de0b74dce49dc69c601705d
Nikita Ustinov <unv@plm-ural.ru>
2024-06-17 14:08:23 +0500

    Add: added Validating Ctrl interface

---
+++ TaskModule/Validation/IValidatingControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskModule.Validation
+{
+    /// <summary>
+    /// Абстрактный класс валидатора контроллов. Используется валидация после ввода
+    /// </summary>
+    /// <typeparam name="ControlType">Класс контролла, который модифицируется проверкой над значением</typeparam>
+    public interface IValidatingControl<ControlType>
+        where ControlType : Control
+    {
+        ErrorProvider EP { get; set; }
+
+        bool IsValueValid(out string errorMessage);
+
+        void Validating(object sender, CancelEventArgs e);
+    }
+}
