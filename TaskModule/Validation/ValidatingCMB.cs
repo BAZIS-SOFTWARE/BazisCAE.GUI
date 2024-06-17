e93c637b28be6846d1c4eb818a1a07c5b9fba544
Nikita Ustinov <unv@plm-ural.ru>
2024-06-17 14:17:44 +0500

    Add: added validating cmb

---
+++ TaskModule/Validation/ValidatingCMB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskModule.Validation
+{
+    public partial class ValidatingCMB : ComboBox, IValidatingControl<ComboBox>
+    {
+        public ValidatingCMB() { InitializeComponent(); }
+
+        public ValidatingCMB(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        public ErrorProvider EP { get; set; } = new ErrorProvider();
+
+        public bool IsValueValid(out string errorMessage)
+        {
+            if (Text.Equals(string.Empty))
+            {
+                errorMessage = "Поле оставлено пустым";
+                return false;
+            }
+
+            if (Items.Contains(Text))
+            {
+                errorMessage = string.Empty;
+                return true;
+            }
+            errorMessage = "Выбранный вариант не доступен. Вероятно, допущена ошибка при выборе значения";
+            return false;
+        }
+
+        void IValidatingControl<ComboBox>.Validating(object sender, CancelEventArgs e)
+        {
+            string errorMessage = string.Empty;
+            if (!IsValueValid(out errorMessage))
+                e.Cancel = true;
+
+            EP.SetError(this, errorMessage);
+        }
+    }
+}
