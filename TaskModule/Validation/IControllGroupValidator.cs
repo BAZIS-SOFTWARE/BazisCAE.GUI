d83306a3b7865861a469c4dc8d0e821b2028c3a6
Nikita Ustinov <unv@plm-ural.ru>
2024-06-20 11:48:34 +0500

    Add: ValidatingButton
    * It has control collection for check
    * ErrorProvider would be threw to control during validation
    * Controls could be added/removed

---
+++ TaskModule/Validation/IControllGroupValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskModule.Validation
+{
+    public interface IControlGroupValidator
+    {
+        ErrorProvider EP {get;}
+        List<IValidatingControl> ValidatingControls { get;}
+        void AddControl(IValidatingControl ctrl);
+        void RemoveControl(IValidatingControl ctrl);
+        bool ValidateControls();
+    }
+}
---
+++ TaskModule/Validation/ValidatingButton.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskModule.Validation
+{
+    public partial class ValidatingButton : Button, IControlGroupValidator
+    {
+        public ValidatingButton()
+        {
+            InitializeComponent();
+            EP = InitializaErrorProvider();
+            ValidatingControls = new List<IValidatingControl>();
+        }
+
+        public ValidatingButton(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+            EP = InitializaErrorProvider();
+            ValidatingControls = new List<IValidatingControl>();
+        }
+
+        public ErrorProvider EP { get; }
+
+        public List<IValidatingControl> ValidatingControls { get; }
+
+        private ErrorProvider InitializaErrorProvider()
+        {
+            var eP = new ErrorProvider();
+            eP.SetIconAlignment(this, ErrorIconAlignment.MiddleRight);
+            eP.SetIconPadding(this, 2);
+            eP.BlinkRate = 1000;
+            eP.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+            return eP;
+        }
+
+        public void AddControl(IValidatingControl ctrl) => ValidatingControls.Add(ctrl);
+
+        public void RemoveControl(IValidatingControl ctrl) => ValidatingControls.Remove(ctrl);
+
+        public bool ValidateControls() => ValidatingControls.Any(x => x.IsValueValid(EP, out string e));
+
+        public void ValidatingButton_OnClick(object sender, CancelEventArgs cea)
+        {
+            if (!ValidateControls())
+                cea.Cancel = true;
+        }
+    }
+}
