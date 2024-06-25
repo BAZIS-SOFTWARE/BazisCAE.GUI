e5d24534f8f4f73f06442df385d202258a0f34dc
Nikita Ustinov <unv@plm-ural.ru>
2024-06-25 11:25:29 +0500

    Hot-Fix: rewrote number-input handler

--- BaseModule/ControlsLib/Validation/TextBoxValidator.cs
+++ BaseModule/ControlsLib/Validation/TextBoxValidator.cs
@@ -44,11 +44,12 @@ namespace BaseModule.ControlsLib.Validation
                 && Text.Any(x => IligalSymbols.Contains(x)))
                 return GetErrorCheckResult(EP, "Переданная строка пуста или содержит неподдерживаемые символы");
 
-            if (IsInputTypeChosen(TXTBoxInputType.Integer) && IsIntegerValuePass())
-                return GetErrorCheckResult(EP);
+            if (IsInputTypeChosen(TXTBoxInputType.Integer))
+                return HandleIntegerValue(EP);
+                
 
-            if (IsInputTypeChosen(TXTBoxInputType.Float) && IsFloatValuePass())
-                return GetErrorCheckResult(EP);
+            if (IsInputTypeChosen(TXTBoxInputType.Float))
+                return HandleFloatValue(EP);
 
             return GetErrorCheckResult(EP, $"Поле с валидирующим обработчиком типа {InputType} не было обработано ни одним обработчиком." +
                 $"Ошибка в веденном значении или типе обработки");
@@ -62,23 +63,32 @@ namespace BaseModule.ControlsLib.Validation
         private bool IsInputTypeChosen(TXTBoxInputType it) =>
             (InputType & it) != 0;
 
-        private bool IsFloatValuePass()
+        private bool HandleFloatValue(ErrorProvider EP)
         {
-            if (IsInputTypeChosen(TXTBoxInputType.Positive))
-                return IsPassRegExCheck("^(([1-9](\\d{1,}))|(\\d{1}))([.](\\d{1,}))?$")
-                    || IsPassRegExCheck("^(\\d{1})(([.])(\\d{1,}))?([e,E])([+]|[-])(\\d|[1-9]\\d{1,})$");
+            if (Text.Contains(","))
+                return GetErrorCheckResult(EP, "В качестве разделителя целой и дробной части необходимо использовать точку");
 
-            return IsPassRegExCheck("^(\\d{1})(([.])(\\d{1,}))?([e,E])([+]|[-])(\\d|[1-9]\\d{1,})$")
-            || IsPassRegExCheck("^([-]?)(([1-9](\\d{1,}))|(\\d{1}))([.](\\d{1,}))?$")
-            || IsPassRegExCheck("^([-]?)(\\d{1})(([.])(\\d{1,}))?([e,E])([+]|[-])(\\d|[1-9]\\d{1,})$");
+            if (IsInputTypeChosen(TXTBoxInputType.Positive)
+                && (IsPassRegExCheck("^(([1-9](\\d{1,}))|(\\d{1}))([.](\\d{1,}))?$")
+                || IsPassRegExCheck("^(\\d{1})(([.])(\\d{1,}))?([e,E])([+]|[-])(\\d|[1-9]\\d{1,})$")))
+                return GetErrorCheckResult(EP);
+
+            else if (IsPassRegExCheck("^([-]?)(([1-9](\\d{1,}))|(\\d{1}))([.](\\d{1,}))?$")
+            || IsPassRegExCheck("^([-]?)(\\d{1})(([.])(\\d{1,}))?([e,E])([+]|[-])(\\d|[1-9]\\d{1,})$"))
+                return GetErrorCheckResult(EP);
+
+            return GetErrorCheckResult(EP, "Числовое поле не прошло проверку. Присутсвуют неопределимые ошибки записи числа");
         }
 
-        private bool IsIntegerValuePass()
+        private bool HandleIntegerValue(ErrorProvider EP)
         {
-            if (IsInputTypeChosen(TXTBoxInputType.Positive))
-                return IsPassRegExCheck("^(([1-9]{1})(\\d{1,})?)$");
+            if (IsInputTypeChosen(TXTBoxInputType.Positive) && IsPassRegExCheck("^(([1-9]{1})(\\d{1,})?)$"))
+                return GetErrorCheckResult(EP);
 
-            return IsPassRegExCheck("^([-]?)(([1-9]{1})(\\d{1,})?)$");
+            else if (IsPassRegExCheck("^([-]?)(([1-9]{1})(\\d{1,})?)$"))
+                return GetErrorCheckResult(EP);
+
+            return GetErrorCheckResult(EP, "Числовое поле не прошло проверку. Присутсвуют неопределимые ошибки записи числа ошибки записи числа");
         }
 
         private bool IsPassRegExCheck(string regEx) => Regex.IsMatch(Text, regEx);
