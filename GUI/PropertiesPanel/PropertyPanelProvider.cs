128d672568c9cfe6abd19c922b1c00ec12a7c24a
Андрей Гомонец <gav@plm-ural.ru>
2025-03-07 14:21:39 +0500

    Fix: Color display (Color [DarkGrey]-->DarkGrey)

--- GUI/PropertiesPanel/PropertyPanelProvider.cs
+++ GUI/PropertiesPanel/PropertyPanelProvider.cs
@@ -5,6 +5,7 @@ using BaseModule.PropertiesPanel;
 using System.Collections.Generic;
 using Model.Interfaces;
 using Model.Interfaces.ObjectsCollections;
+using System.Drawing;
 
 namespace BazisGUI.PropertiesPanel
 {
@@ -20,15 +21,40 @@ namespace BazisGUI.PropertiesPanel
 
         public void DrawPropertyOnPanel(ISetInfo obj) // создание коллекции RowProperty и отправка внутри EventArgs в PropertyPanel.DataGridView
         {
+            
             List<RowProperty> list = new List<RowProperty>()
             {
                 new RowProperty("   Имя",obj.Name, () => {}),
-                new RowProperty("   Цвет",obj.Color, () => {}),
+                new RowProperty("   Цвет",obj.Color.Name, () => {}),
                 new RowProperty("   Тип",obj.ObjType, () => {})
             };
 
             Out(new DrowPropertyOnPanelEventArgs(list));
-        } 
-
+        }
+        ////Русская локализация
+        //private string GetRussianColorName(Color color)
+        //{
+        //    return colorNames.TryGetValue(color, out string name) ? name : "Неизвестный цвет";
+        //}
+        //Dictionary<Color, string> colorNames = new Dictionary<Color, string>
+        //{
+        //    { Color.Black, "Чёрный" },
+        //    { Color.White, "Белый" },
+        //    { Color.Red, "Красный" },
+        //    { Color.Green, "Зелёный" },
+        //    { Color.Blue, "Синий" },
+        //    { Color.Yellow, "Жёлтый" },
+        //    { Color.Gray, "Серый" },
+        //    { Color.DarkGray, "Тёмно-серый" },
+        //    { Color.LightGray, "Светло-серый" },
+        //    { Color.Orange, "Оранжевый" },
+        //    { Color.Pink, "Розовый" },
+        //    { Color.Purple, "Фиолетовый" },
+        //    { Color.Brown, "Коричневый" },
+        //    { Color.Cyan, "Голубой" },
+        //    { Color.Magenta, "Пурпурный" },
+        //    { Color.Lime, "Лаймовый" },
+        //    { Color.Olive, "Оливковый" }
+        //};
     }
 }
