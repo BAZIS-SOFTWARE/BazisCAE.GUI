fa84cb0947769bcb204fb96319b7de03f2af307b
Victor Kapkaev <78375906+sadovnichek@users.noreply.github.com>
2025-03-12 10:54:28 +0500

    126 добавление панели визуализации граничных условий (#135)
    * реализован control для отображения диаграммы Ганта
    
    * реализован control для отображения диаграммы Ганта
    
    * автоподгонка шрифта в gantt chart control
    
    * созданы две версии представления диаграмы ганта: линейный сприсок и дерево
    
    * рефакторинг GanttChartModel
    
    ---------
    
    Co-authored-by: Georgy <sprinkfild@mail.ru>

---
+++ GUI/GanttChartModel.cs
@@ -0,0 +1,57 @@
+﻿using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace GanttChart
+{
+    public class GanttChartModel
+    {
+        public Chart Chart { get; private set; }
+
+        public GanttChartModel(double minValue, double maxValue, double intervalLength, int barMaxCount)
+        {
+            Chart = new Chart();
+            Chart.ChartAreas.Add(new ChartArea());
+            ConfigureChart(minValue, maxValue, intervalLength, barMaxCount);
+        }
+
+        public void AddTask(double start, double end, int layer, string label, Color color)
+        {
+            var index = Chart.Series[0].Points.AddXY(layer, start, end);
+            Chart.Series[0].Points[index].Color = color;
+            Chart.Series[0].Points[index].BackSecondaryColor = Color.Transparent;
+            Chart.Series[0].Points[index].AxisLabel = label;
+        }
+
+        public void InverseBarColor(int layer)
+        {
+            var index = layer - 1;
+            if (index >= 0 && index < Chart.Series[0].Points.Count)
+            {
+                var bar = Chart.Series[0].Points[index];
+                (bar.Color, bar.BackSecondaryColor) = (bar.BackSecondaryColor, bar.Color);
+            }
+        }
+
+        private void ConfigureChart(double minValue, double maxValue, double intervalLength, int barMaxCount)
+        {
+            var series = Chart.Series.Add("gantt diagram");
+            series.ChartType = SeriesChartType.RangeBar;
+            series.YValueType = ChartValueType.Double;
+
+            var ax = Chart.ChartAreas[0].AxisX;
+            ax.Minimum = 0;
+            ax.Maximum = barMaxCount + 1;
+            ax.MajorGrid.Enabled = false;
+            ax.IsLabelAutoFit = true;
+            ax.LabelAutoFitStyle = LabelAutoFitStyles.DecreaseFont;
+
+            var ay = Chart.ChartAreas[0].AxisY;
+            ay.Minimum = minValue;
+            ay.Maximum = maxValue + intervalLength;
+            ay.MajorGrid.Interval = intervalLength;
+            ay.LabelStyle.Interval = intervalLength;
+            ay.IsLabelAutoFit = true;
+            ay.LabelAutoFitStyle = LabelAutoFitStyles.LabelsAngleStep30;
+        }
+    }
+}
---
+++ GUI/GanttChartTreeView.cs
@@ -0,0 +1,92 @@
+﻿using Project.Interfaces.Tasks;
+using Project.Tasks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GanttChart
+{
+    public partial class GanttChartTreeView : UserControl
+    {
+        private GanttChartModel ganttChart;
+        private Dictionary<TreeNode, int> mapTreeViewIndexToChartIndex;
+
+        public GanttChartTreeView(List<IValuableData> tasks, int timestamps)
+        {
+            InitializeComponent();
+
+            var start = tasks.Select(t => t.StartTime).Min();
+            var end = tasks.Select(t => t.StopTime).Max();
+            var interval = (end - start) / timestamps;
+
+            ganttChart = new GanttChartModel(start, end, interval, tasks.Count);
+            mapTreeViewIndexToChartIndex = new Dictionary<TreeNode, int>();
+
+            ganttChart.Chart.Dock = DockStyle.Fill;
+            splitContainer.Panel2.Controls.Add(ganttChart.Chart);
+
+            ProcessTasks(tasks);
+        }
+
+        private void ProcessTasks(List<IValuableData> tasks)
+        {
+            var chartLayer = 1;
+            for (var i = 0; i < tasks.Count(); i++)
+            {
+                var groupName = tasks[i].Name.Split('-')[0];
+                var fullName = tasks[i].Name;
+
+                if (!treeView.Nodes.ContainsKey(groupName))
+                {
+                    var groupNode = treeView.Nodes.Add(groupName, groupName);
+                    groupNode.Checked = true;
+                }
+
+                var parent = treeView.Nodes[groupName];
+                var node = parent.Nodes.Add(fullName);
+                mapTreeViewIndexToChartIndex.Add(node, chartLayer);
+                node.Checked = true;
+
+                ganttChart.AddTask(tasks[i].StartTime, tasks[i].StopTime, chartLayer, fullName, MapTaskToColor(tasks[i]));
+                chartLayer++;
+            }
+        }
+
+        private Color MapTaskToColor(IValuableData task)
+        {
+            switch (task)
+            {
+                case ClampData _: return Color.FromArgb(56, 94, 157);
+                case HeatData _: return Color.FromArgb(194, 110, 96);
+                case LoadData _: return Color.FromArgb(57, 157, 152);
+                case MatData _: return Color.FromArgb(157, 57, 95);
+                case MediaData _: return Color.FromArgb(57, 157, 85);
+                default: return Color.FromArgb(152, 57, 157);
+            }
+        }
+
+        private void treeView_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            if (!(sender is TreeView treeView))
+                return;
+            if (mapTreeViewIndexToChartIndex.TryGetValue(e.Node, out var layer))
+            {
+                ganttChart.InverseBarColor(layer);
+            }
+            else
+            {
+                foreach (TreeNode childNode in e.Node.Nodes)
+                {
+                    if (childNode.Checked != e.Node.Checked)
+                        childNode.Checked = e.Node.Checked;
+                }
+            }
+        }
+    }
+}
