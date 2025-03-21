7c1e45d7c52f98191d135fe754a61f9634c22b42
kapik95 <48057627+kapik95@users.noreply.github.com>
2025-03-21 12:27:43 +0500

    141.1 добавление функционала редактирования для панели свойств (#149)
    * Update: Editing the color of elements
    
    * Update: ComboBox for display added (there are bugs)
    
    * Test
    
    * Update: Modification of the table construction method
    
    * Update RowProperty
    
    * Update: structure change <Property Panel>
    
    * Add: Properties panel implemented for Object
    
    * Add: Functionality of the properties panel for the group of objects
    
    * Correction of property panel errors
    
    * fix
    
    * Исправил отображение таблицы в PropertyPanel (убрал пустую строку внизу)
    
    * Add: Tests for Property Panel (SelectingTreeElementsTests and RenameTreeElementsTests)
    
    * Add test 'SelectColorAndTypeModeTest'
    
    * Remove: unused deps
    
    * Fix method 'SwapeViewMode'
    
    * Fix
    
    ---------
    
    Co-authored-by: Nikita Ustinov <unv@plm-ural.ru>

---
+++ TestGUI/PropertyPanelTests.cs
@@ -0,0 +1,198 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Appium;
+using static TestGUI.TestProvider;
+using OpenQA.Selenium.Interactions;
+using System.Runtime.InteropServices;
+using NUnit.Framework.Internal.Execution;
+using OpenQA.Selenium;
+
+namespace TestGUI
+{
+
+    public class PropertyPanelTests
+    {
+        [Test(Description = "Последовательное развертывание корней 'Объекты', 'ГруппыОбъектов' и 'Данные'")]
+        public void SelectingTreeElementsTest()
+        {
+            var opt = new AppiumOptions();
+            opt.AddAdditionalCapability("app", Path.GetFullPath(@".\..\..\..\..\GUI\bin\x64\Debug\BazisGUI.exe"));
+            opt.AddAdditionalCapability("ms:waitForAppLaunch", "5");
+            opt.AddAdditionalCapability("appArguments", $"-proj {Path.GetFullPath(@".\..\..\..\..\GUI\Projects\Welding\Arc\proj.bpf")}");
+            opt.PlatformName = "Windows11x64";
+            var url = new Uri("http://127.0.0.1:4723");
+            var wd = new WindowsDriver<WindowsElement>(url, opt);
+
+            try
+            {
+                var actions = new Actions(wd);
+
+                TestProvider.GetElement(wd, "Модули", SearchWay.Name).Click();
+                TestProvider.GetElement(wd, "Сварка", SearchWay.Name).Click();
+                TestProvider.GetElement(wd, "Объекты", SearchWay.Name).Click();
+ 
+                ExpandElement(wd, actions, "Элементы2D");
+                ExpandElement(wd, actions, "Элементы3D");
+                ExpandElement(wd, actions, "Группы объектов");
+                ExpandElement(wd, actions, "Данные");
+                ExpandElement(wd, actions, "Данные");
+
+                actions.Perform();
+
+                TestProvider.GetElement(wd, "Элемент2D : 7384", SearchWay.Name).Click();
+                TestProvider.GetElement(wd, "Элемент3D : 23258", SearchWay.Name).Click();
+                TestProvider.GetElement(wd, "refLine", SearchWay.Name).Click();
+                TestProvider.GetElement(wd, "Load", SearchWay.Name).Click();
+                TestProvider.GetElement(wd, "Среда : air Коэф.теплоотдачи.воздух 20 0 1500 *", SearchWay.Name).Click();
+                TestProvider.GetElement(wd, "Нагрев : ARC;4;100;25 Load 0 50 baseLine|refLine;2;startNodes;startNodes;0|0|0|0", SearchWay.Name).Click();
+            }
+            catch (Exception e) { wd.CloseApp(); Assert.Fail(e.Message); }
+
+            finally { wd.CloseApp(); }
+        }
+
+        [Test(Description = "Изменение имени объекта - попытка присвоения некорректного имени и ввод валидных значений")]
+        [TestCase("Элемент2D : 7384" )]
+        [TestCase("refLine")]
+        //[TestCase("Среда : air Коэф.теплоотдачи.воздух 20 0 1500 *")]
+        public void RenameTreeElementsTest(string element)
+        {
+            var opt = new AppiumOptions();
+            opt.AddAdditionalCapability("app", Path.GetFullPath(@".\..\..\..\..\GUI\bin\x64\Debug\BazisGUI.exe"));
+            opt.AddAdditionalCapability("ms:waitForAppLaunch", "5");
+            opt.AddAdditionalCapability("appArguments", $"-proj {Path.GetFullPath(@".\..\..\..\..\GUI\Projects\Welding\Arc\proj.bpf")}");
+            opt.PlatformName = "Windows11x64";
+            var url = new Uri("http://127.0.0.1:4723");
+            var wd = new WindowsDriver<WindowsElement>(url, opt);
+
+            try
+            {
+                var actions = new Actions(wd);
+
+                TestProvider.GetElement(wd, "Модули", SearchWay.Name).Click();
+                TestProvider.GetElement(wd, "Сварка", SearchWay.Name).Click();
+
+                ExpandElement(wd, actions, "Элементы2D");
+                ExpandElement(wd, actions, "Элементы3D");
+                ExpandElement(wd, actions, "Группы объектов");
+                actions.Perform();
+
+                TestProvider.GetElement(wd, element, SearchWay.Name).Click();
+
+                Rename(wd, "name@1 231", true);
+                Rename(wd, "     name@1231", true);
+                Rename(wd, "name@1231     ", true);
+                Rename(wd, "new_nameElement", false);
+                Rename(wd, "newNameElement232425!@#$%^&*()_+=", false);
+            }
+            catch (Exception e) { wd.CloseApp(); Assert.Fail(e.Message); }
+
+            finally { wd.CloseApp(); }
+        }
+
+        [Test(Description = "Изменение цвета и последовательное изменение режима отображения объекта")]
+        [TestCase("Элемент2D : 7384")]
+        [TestCase("Элемент3D : 23258")]
+        public void SelectColorAndTypeModeTest(string element)
+        {
+            var opt = new AppiumOptions();
+            opt.AddAdditionalCapability("app", Path.GetFullPath(@".\..\..\..\..\GUI\bin\x64\Debug\BazisGUI.exe"));
+            opt.AddAdditionalCapability("ms:waitForAppLaunch", "5");
+            opt.AddAdditionalCapability("appArguments", $"-proj {Path.GetFullPath(@".\..\..\..\..\GUI\Projects\Welding\Arc\proj.bpf")}");
+            opt.PlatformName = "Windows11x64";
+            var url = new Uri("http://127.0.0.1:4723");
+            var wd = new WindowsDriver<WindowsElement>(url, opt);
+
+            try
+            {
+                var actions = new Actions(wd);
+
+                TestProvider.GetElement(wd, "Модули", SearchWay.Name).Click();
+                TestProvider.GetElement(wd, "Сварка", SearchWay.Name).Click();
+
+                //Собираю в actions действия которые нужно выполнить 
+                ExpandElement(wd, actions, "Элементы2D");
+                ExpandElement(wd, actions, "Элементы3D");
+                actions.Perform();
+
+                //Ячейка имя 
+                TestProvider.GetElement(wd, element, SearchWay.Name).Click();
+
+                SwapeColor(wd, -10, 100);
+                SwapeColor(wd, -125, 100);
+
+                SwapeViewMode(wd, 1);
+                SwapeViewMode(wd, 2);
+                SwapeViewMode(wd, 3);
+            }
+            catch (Exception e) { wd.CloseApp(); Assert.Fail(e.Message); }
+
+            finally { wd.CloseApp(); }
+        }
+
+        private static void Rename(WindowsDriver<WindowsElement> wd, string newName, bool isError)
+        {
+            WindowsElement exept = null;
+            ClickCell(wd, " Строка 0, Не отсортировано.");
+            wd.Keyboard.SendKeys(newName + OpenQA.Selenium.Keys.Enter);
+            try
+            {
+                exept = wd.FindElement(By.Name("FormatException"));
+                Thread.Sleep(1000);
+            }
+            catch(OpenQA.Selenium.WebDriverException ex) { }
+            
+            if (isError) 
+            {
+                Assert.That(!(exept is null));
+                wd.Keyboard.SendKeys(OpenQA.Selenium.Keys.Enter);
+            }
+            else
+            {
+                if (!(exept is null))
+                {
+                    wd.Keyboard.SendKeys(newName + OpenQA.Selenium.Keys.Enter);
+                    throw new Exception("FormatException: ");
+                }
+            }
+        }
+
+        private static void SwapeViewMode(WindowsDriver<WindowsElement> wd, int indexViewMode)
+        {
+            TestProvider.GetElement(wd, " Строка 2, Не отсортировано.", SearchWay.Name).Click();
+            TestProvider.ClickByOffset(wd, 275, 0, ClickType.LeftOne);
+            TestProvider.ClickByOffset(wd, 0, 0, ClickType.LeftOne);
+            var selectedViewMode = new Actions(wd);
+            for (int i = indexViewMode - 1; i < indexViewMode; i++)
+            {
+                selectedViewMode.SendKeys(OpenQA.Selenium.Keys.ArrowDown);
+            }
+            selectedViewMode.SendKeys(OpenQA.Selenium.Keys.Enter).Perform();
+            TestProvider.ClickByOffset(wd, 100, 0, ClickType.LeftOne);
+            var update = new Actions(wd);
+            update.SendKeys(OpenQA.Selenium.Keys.Escape).Perform();
+        }
+
+        private static void SwapeColor(WindowsDriver<WindowsElement> wd, int offsetX, int offsetY)
+        {
+            ClickCell(wd, " Строка 1, Не отсортировано.");
+            TestProvider.GetElement(wd, "Основные цвета:", SearchWay.Name).Click();
+            TestProvider.ClickByOffset(wd, offsetX, offsetY, ClickType.LeftOne);
+            TestProvider.GetElement(wd, "ОК", SearchWay.Name).Click();
+            var update = new Actions(wd);
+            update.SendKeys(OpenQA.Selenium.Keys.Escape).Perform();
+        }
+
+        private static void ClickCell(WindowsDriver<WindowsElement> wd, string rowName)
+        {
+            TestProvider.GetElement(wd, rowName, SearchWay.Name).Click();
+            TestProvider.ClickByOffset(wd, 130, 0, ClickType.LeftOne);
+            TestProvider.ClickByOffset(wd, 0, 0, ClickType.LeftOne);
+        }
+
+        private static void ExpandElement(WindowsDriver<WindowsElement> wd, Actions actions, string nameInTree)    
+        {
+            var element = TestProvider.GetElement(wd, nameInTree, SearchWay.Name);
+            actions.DoubleClick(element);
+        }
+    }
+}
