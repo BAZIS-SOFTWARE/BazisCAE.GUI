276d12564a95865c3abc6d681a717015859eb91e
sadovnichek <kapkaevictor@gmail.com>
2025-03-27 17:29:34 +0500

    внедрение диаграммы ганта

--- GUI/GanttChartTreeView.cs
+++ GUI/GanttChartTreeView.cs
@@ -10,37 +10,37 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GanttChart
+namespace BazisGUI
 {
     public partial class GanttChartTreeView : UserControl
     {
         private GanttChartModel ganttChart;
-        private Dictionary<TreeNode, int> mapTreeViewIndexToChartIndex;
+        private Dictionary<TreeNode, int> mapTreeNodeToChartIndex;
 
         public GanttChartTreeView(List<IValuableData> tasks, int timestamps)
         {
             InitializeComponent();
 
-            var start = tasks.Select(t => t.StartTime).Min();
-            var end = tasks.Select(t => t.StopTime).Max();
+            var start = tasks.Min(t => t.StartTime);
+            var end = tasks.Max(t => t.StopTime);
             var interval = (end - start) / timestamps;
 
             ganttChart = new GanttChartModel(start, end, interval, tasks.Count);
-            mapTreeViewIndexToChartIndex = new Dictionary<TreeNode, int>();
+            mapTreeNodeToChartIndex = new Dictionary<TreeNode, int>();
 
             ganttChart.Chart.Dock = DockStyle.Fill;
             splitContainer.Panel2.Controls.Add(ganttChart.Chart);
 
-            ProcessTasks(tasks);
+            AddTasks(tasks);
         }
 
-        private void ProcessTasks(List<IValuableData> tasks)
+        private void AddTasks(List<IValuableData> tasks)
         {
             var chartLayer = 1;
             for (var i = 0; i < tasks.Count(); i++)
             {
-                var groupName = tasks[i].Name.Split('-')[0];
-                var fullName = tasks[i].Name;
+                var groupName = tasks[i].Name;
+                var description = tasks[i].GetInfo;
 
                 if (!treeView.Nodes.ContainsKey(groupName))
                 {
@@ -49,11 +49,11 @@ namespace GanttChart
                 }
 
                 var parent = treeView.Nodes[groupName];
-                var node = parent.Nodes.Add(fullName);
-                mapTreeViewIndexToChartIndex.Add(node, chartLayer);
+                var node = parent.Nodes.Add(description);
+                mapTreeNodeToChartIndex.Add(node, chartLayer);
                 node.Checked = true;
 
-                ganttChart.AddTask(tasks[i].StartTime, tasks[i].StopTime, chartLayer, fullName, MapTaskToColor(tasks[i]));
+                ganttChart.AddTask(tasks[i].StartTime, tasks[i].StopTime, chartLayer, groupName, MapTaskToColor(tasks[i]), description);
                 chartLayer++;
             }
         }
@@ -62,12 +62,12 @@ namespace GanttChart
         {
             switch (task)
             {
-                case ClampData _: return Color.FromArgb(56, 94, 157);
+                case ClampData _: return Color.FromArgb(194, 174, 95);
                 case HeatData _: return Color.FromArgb(194, 110, 96);
                 case LoadData _: return Color.FromArgb(57, 157, 152);
                 case MatData _: return Color.FromArgb(157, 57, 95);
                 case MediaData _: return Color.FromArgb(57, 157, 85);
-                default: return Color.FromArgb(152, 57, 157);
+                default: return Color.FromArgb(61, 81, 160);
             }
         }
 
@@ -75,7 +75,7 @@ namespace GanttChart
         {
             if (!(sender is TreeView treeView))
                 return;
-            if (mapTreeViewIndexToChartIndex.TryGetValue(e.Node, out var layer))
+            if (mapTreeNodeToChartIndex.TryGetValue(e.Node, out var layer))
             {
                 ganttChart.InverseBarColor(layer);
             }
