24127f32713a9512385f1a0248f551ee1f8781f6
kapik96rus <andrei.gomonec@gmail.com>
2025-05-22 12:33:13 +0500

    feat : Добавлен контрол для среды

---
+++ BaseModule/Tasks/TasksFromNavigator/Controls/MediaControlCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaseModule.Tasks.TasksFromNavigator.Controls
+{
+    public partial class MediaControlCreator: UserControl
+    {
+        public MediaControlCreator()
+        {
+            InitializeComponent();
+        }
+    }
+}
