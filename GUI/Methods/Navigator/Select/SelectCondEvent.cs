7c4b01bfcd08940eecab3804f47e05847ad80254
Биленко Георгий Андреевич <sprinkfild@mail.ru>
2025-08-12 11:05:50 +0500

    слияние

--- GUI/Methods/Navigator/Select/SelectCondEvent.cs
+++ GUI/Methods/Navigator/Select/SelectCondEvent.cs
@@ -5,7 +5,6 @@ using PropertiesCalculator.FunctionData;
 using PropertiesCalculator.MaterialData;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 using System.Drawing;
 using System.Linq;
 
