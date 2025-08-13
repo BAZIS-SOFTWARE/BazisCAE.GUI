15353207cfd0bd8f8afe9f42e40b3ffc1d457346
kapik96rus <andrei.gomonec@gmail.com>
2025-08-13 15:46:32 +0500

    upd : Перемещение 1Д, 2Д, 3Д объектов в соответствии с изменением координат узла

--- GUI/Methods/PropertyPanel/ChangeModelObjectProperty.cs
+++ GUI/Methods/PropertyPanel/ChangeModelObjectProperty.cs
@@ -1,6 +1,9 @@
 ﻿using BaseModule.Navigator;
 using BaseModule.PropertiesPanel;
 using BazisGUI.Utilities;
+using Model.Interfaces;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace BazisGUI
@@ -25,19 +28,19 @@ namespace BazisGUI
                     var c = mObj.GetCoordinates().First();
 
                     if (obj.Header == "Координата X")
-                    {
                         c._x = float.Parse(obj.NewValue);
-                    }
-
                     else if (obj.Header == "Координата Y")
                         c._y = float.Parse(obj.NewValue);
                     else
                         c._z = float.Parse(obj.NewValue);
+                    List<ObjType> objTypes = new List<ObjType> { ObjType.Узел, ObjType.Элемент1D, ObjType.Элемент2D, ObjType.Элемент3D };
+                    foreach(var type in objTypes)
+                    {
+                        var presentor = project.CreateModelObjectsPresentor(type);
+                        SetVBObjectAttribute(presentor, "координаты");
+                    }
                 }
                 navigator.SelectedNode.Text = mObj.ToString();
-                var presentor = project.CreateModelObjectsPresentor(mObj.ObjType);
-                SetVBObjectAttribute(presentor, "координаты");
-
                 DisplayObjects();
             }
         }
