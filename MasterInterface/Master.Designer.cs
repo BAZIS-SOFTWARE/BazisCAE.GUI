87055c471d31e7fa4bbdaef83d8e7a766e410480
Ustinov_Nikita <unv@plm-ural.ru>
2025-12-17 15:41:43 +0500

    UPD: master design

--- MasterInterface/Master.Designer.cs
+++ MasterInterface/Master.Designer.cs
@@ -28,9 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            SuspendLayout();
+            // 
+            // Master
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Name = "Master";
+            Size = new Size(800, 450);
+            ResumeLayout(false);
         }
 
         #endregion
--- MasterInterface/Master.cs
+++ MasterInterface/Master.cs
@@ -4,10 +4,20 @@ namespace MasterInterface
 {
     public abstract partial class Master : UserControl, IMaster
     {
-        public Master()
-        {
-            InitializeComponent();
-        }
+        /// <summary>
+        /// Названия материалов
+        /// </summary>
+        private List<string> materials = new List<string>();
+
+        /// <summary>
+        /// Названия функций
+        /// </summary>
+        private List<string> functions = new List<string>();
+
+        /// <summary>
+        /// Сгруппированные по типу группы (их имена)
+        /// </summary>
+        private Dictionary<ObjType, List<string>> groups = new Dictionary<ObjType, List<string>>();
 
         public event Action<string, Color> PrintInfoEvent;
         public event Action<string[]> SubmintParametrizedStringsEvent;
@@ -37,7 +47,17 @@ namespace MasterInterface
 
         public virtual void InitialMasterFilling(IEnumerable<string> materials, IEnumerable<string> functions, Dictionary<ObjType, List<string>> groupsByObjType)
         {
-            throw new NotImplementedException();
+            this.materials.Clear();
+            foreach (var material in materials)
+                this.materials.Add(material);
+
+            this.functions.Clear();
+            foreach (var function in functions)
+                this.functions.Add(function);
+
+            groups.Clear();
+            foreach(var key in groupsByObjType.Keys)
+                groups[key] = groupsByObjType[key].ToList();
         }
 
         public virtual void RenameGroup(ObjType type, string oldName, string newName)
