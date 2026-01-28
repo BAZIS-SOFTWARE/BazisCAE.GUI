2419fc064f24537fd34af844f92ed249756e74c9
agmnts <andrei.gomonec@gmail.com>
2026-01-28 18:22:46 +0500

    feat : реализован выбор сопряженных элементов для геометрии

--- GUI/Methods/Navigator/Object/SelectAdjacencies.cs
+++ GUI/Methods/Navigator/Object/SelectAdjacencies.cs
@@ -1,21 +1,56 @@
-﻿namespace BazisGUI
+﻿using Model.Interfaces;
+using Model.Interfaces.ObjectsCollections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BazisGUI
 {
     public partial class BaseForm
     {
-        private void SelectAdj(int dim, int number)
+        private List<(ISetInfo, List<int> Numbers)> SelectAdj(int dim, int number)
         {
-            var adgTags = GmshController.Gmsh.Model.GetAdjacencies(dim, number);
-
-            //var upperLevel = adgTags.Item1;
-            //var lowerLevel = adgTags.Item2;
-
-            foreach(var array in new[] { adgTags.Item1, adgTags.Item2 })
+            var (upperLevel, lowerLevel) = GmshController.Gmsh.Model.GetAdjacencies(dim, number);
+            var dimUp = dim + 1;
+            var dimLow = dim - 1;
+            var adgTypeUp = (ObjType)dimUp;
+            var adgTypeLow = (ObjType)dimLow;
+            List<(ISetInfo, List<int> Numbers)> sets = new List<(ISetInfo, List<int> Numbers)>();
+            var setU = project.GetModelSetInfo(adgTypeUp, adgTypeUp.ToString());
+            if(setU != null)
             {
-                foreach(var item in array)
-                {
-
-                }
+                List<int> numbers = new List<int>();
+                foreach (var set in setU.GetNumbers())
+                    if (Array.Exists(upperLevel, x => x == set))
+                        numbers.Add(set);
+                
+                sets.Add((setU, numbers));
             }
+
+            var setL = project.GetModelSetInfo(adgTypeLow, adgTypeLow.ToString());
+            if(setL != null)
+            {
+                List<int> numbers = new List<int>();
+
+                foreach (var set in setL.GetNumbers())
+                    if (Array.Exists(lowerLevel, x => x == set))
+                        numbers.Add(set);
+                sets.Add((setL, numbers));
+            }
+
+            return sets;
+        }
+
+
+        private (ISetInfo Set, List<int> Numbers) GetFilteredSet(int dim, int[] adjNumbers)
+        {
+            var setInfo = project.GetModelSetInfo((ObjType)dim, ((ObjType)dim).ToString());
+            if (setInfo == null) return (null, new List<int>());
+
+            var adjSet = new HashSet<int>(adjNumbers);
+            var numbers = setInfo.GetNumbers().Where(n => adjSet.Contains(n)).ToList();
+
+            return (setInfo, numbers);
         }
     }
 }
--- GUI/Methods/Scene/ContextMenu/SelectAdjac.cs
+++ GUI/Methods/Scene/ContextMenu/SelectAdjac.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model.Interfaces.ObjectsCollections;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace BazisGUI
@@ -9,13 +11,24 @@ namespace BazisGUI
         {
             try
             {
+                List<List<(ISetInfo, List<int> Numbers)>> all = new List<List<(ISetInfo, List<int> Numbers)>>();
+
                 foreach (var item in GetModelObjects(SelectedObjects).Where(x => x.Color == settingsConfig.SelectObjectColor))
                 {
                     var dim = (int)item.ObjType;
-                    SelectAdj(dim, item.Number);
+                    var sets = SelectAdj(dim, item.Number);
+                    all.Add(sets);
                 }
-
-                // Обновить отображение после установки цвета у всех сопряжённых
+                foreach (var sets in all)
+                {
+                    foreach(var (set, numbers) in sets)
+                    {
+                        foreach(var number in numbers)
+                            set.SetColor(settingsConfig.SelectObjectColor, number);
+                        var pres = project.CreateModelObjectsPresentor(set);
+                        SetVBObjectAttribute(pres, "цвет");
+                    }
+                }
                 DisplayObjects();
             }
             catch (Exception ex)
