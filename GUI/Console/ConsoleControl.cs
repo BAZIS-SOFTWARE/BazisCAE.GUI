7b4fe09aafc888bc0d9750f57c889b32aa8d1db4
Andrey Vital'evich <48057627+agmnts@users.noreply.github.com>
2026-02-03 09:54:36 +0500

    280 хранение выполненных команд и их повторный вызов в консоли (#288)
    * feat : реализация сохранения истории консоли
    
    * refactor: исправлены команды консоли. Отсутсвовал поворот модели

--- GUI/Console/ConsoleControl.cs
+++ GUI/Console/ConsoleControl.cs
@@ -75,7 +75,7 @@ namespace BazisGUI.Console
             { "Перенумерация сетки",GenCmd.RenumberMesh},
             { "Переместить узел",GenCmd.MoveNodes},
             { "Переместить сетку",GenCmd.MoveMesh},
-            { "Повернуть сетку",GenCmd.MoveMesh},
+            { "Повернуть сетку",GenCmd.RotateMesh},
             { "Найти свободные узлы",GenCmd.FindFreeNodes},
             { "Найти совпадающие",GenCmd.FindCoincident},
             { "Найти объемные элементы",GenCmd.FindVolElems},
@@ -193,6 +193,20 @@ namespace BazisGUI.Console
 
         }
 
+        public void PrintHistory(string str)
+        {
+            int lastLineIndex = rtxbField.Lines.Length - 1;
+            int startIndex = rtxbField.GetFirstCharIndexFromLine(lastLineIndex);
+
+            rtxbField.SelectionStart = startIndex;
+            rtxbField.SelectionLength = rtxbField.TextLength - startIndex;
+
+            rtxbField.SelectedText = str;
+
+            rtxbField.SelectionStart = rtxbField.TextLength;
+            rtxbField.SelectionLength = 0;
+        }
+
         void HighlightPhrase(string phrase, Color color)
         {
             int pos = rtxbField.SelectionStart;
@@ -242,6 +256,8 @@ namespace BazisGUI.Console
                 if (subCmds[genCmds[cmds[0]]].Length !=  cmds.Count -1)
                     throw new Exception("Неверное колличество аргументов");
 
+                ConsoleHistory.AddComand(line);
+                
                 switch (genCmds[cmds[0]])
                 {
                     case GenCmd.FindObject:
@@ -259,10 +275,10 @@ namespace BazisGUI.Console
                         RenumberMeshEvent?.Invoke(this, new ModelRenumberEventArgs(cmds[1]));
                         break;
                     case GenCmd.MoveMesh:
-                        if(cmds[1] == "переместить")
-                            ModelShiftCoordinateEvent?.Invoke(this, new ModelShiftCoordinateEventArgs(cmds[2]));
-                        else
-                            ModelRotateEvent?.Invoke(this, new ModelRotateEventArgs(cmds[2]));
+                        ModelShiftCoordinateEvent?.Invoke(this, new ModelShiftCoordinateEventArgs(cmds[2]));
+                        break;
+                    case GenCmd.RotateMesh:
+                        ModelRotateEvent?.Invoke(this, new ModelRotateEventArgs(cmds[2]));
                         break;
                     case GenCmd.MoveNodes:
                         if (cmds[1] == "переместить")
@@ -361,7 +377,7 @@ namespace BazisGUI.Console
 
             rtxbField.BackColor = colorDialog.Color;
         }
- 
+
 
         private void rtxbField_KeyDown(object sender, KeyEventArgs e)
         {
@@ -369,7 +385,7 @@ namespace BazisGUI.Console
             {
                 var cmds = rtxbField.Lines[rtxbField.Lines.Count() - 1];
 
-                if(cmds.Length != 0)
+                if (cmds.Length != 0)
                 {
                     trd = new Thread(delegate ()
                     {
@@ -394,6 +410,14 @@ namespace BazisGUI.Console
 
                     rtxbField.AppendText($"\n < {cmds}");
                 }
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                PrintHistory(ConsoleHistory.GetPreviousCommand());
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                PrintHistory(ConsoleHistory.GetNextCommand());
             }
         }
     }
