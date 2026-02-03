74128ca949f1b8f665d6b05b1adb01d32aa57e28
Ustinov_Nikita <unv@plm-ural.ru>
2026-02-03 10:16:10 +0500

    ADD: show model after result reflection form closed

--- GUI/Methods/Results/Reflect.cs
+++ GUI/Methods/Results/Reflect.cs
@@ -79,7 +79,14 @@ namespace BazisGUI
                         btn.Checked = false;
                         HideGeometryObj("DisplayReflectionPlane");
                         HideGeometryObj("DisplayBoundingBox");
-                        VBOController.DeleteAllVBObjects();
+                        if (VBOController.GetVBObjs().Count() > 0)
+                        {
+                            // TODO
+                            // определить: необходимо показывать все объекты или только нескрытые
+                            // скорректировать решение на основе выводов выше
+                            VBOController.DeleteAllVBObjects();
+                            CreateVBObjects("Объекты");
+                        }
                         DisplayObjects();
                     };
                     reflectForm.Show();
