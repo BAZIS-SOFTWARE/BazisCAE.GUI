b392953bd9ac2434c5812769f03e9fd2f68750eb
agmnts <andrei.gomonec@gmail.com>
2026-03-03 17:10:48 +0500

    feat : реализовано создание группы набора через панель свойств

--- GUI/Methods/Navigator/Sets/SelectSetEvent.cs
+++ GUI/Methods/Navigator/Sets/SelectSetEvent.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BazisGUI
 {
@@ -21,36 +22,37 @@ namespace BazisGUI
                 // пока заглушим обработку объема
                 if (arg2.Split(' ')[0].TryToEnum(out objType))
                 {
-                    if (objType == ObjType.Узел | objType == ObjType.Элемент1D
-                    | objType == ObjType.Элемент2D | objType == ObjType.Элемент3D)
+                    if (objType == ObjType.Узел | objType == ObjType.Элемент1D | objType == ObjType.Элемент2D | objType == ObjType.Элемент3D)
                     {
                         //var objType = Converters.ConvertNavigatorNodeNameToObjType(arg1);
-
                         var set = project.GetModelSetInfo(objType, setName);
                         if(set != null)
                         {
                             var rows = GetSetProperty(set);
 
-
                             if (objType != ObjType.Узел)
                             {
-                                rows.Add(new RowProperty("Порядок точности",
-                                new DropDownPropertyValue("",
-                                new List<string>() { "1", "2" })));
-                                rows.Add(new RowProperty("Смежные узлы", new ButtonPropertyValue("Показать",
-                                () => {
-                                ShowAdjacenciesSet(objType, setName);
-                                DisplayObjects();
+                                var firstObj = project.GetModelObject(objType, set.GetNumbers().First());
+                                var level = project.GetModelElements(firstObj.Dim, setName).FirstOrDefault().Level;   
+
+                                rows.Add(new RowProperty("Порядок точности",new DropDownPropertyValue(level,new List<string>() { "1", "2" })));
+                                rows.Add(new RowProperty("Смежные узлы", new ButtonPropertyValue("Показать",() => {ShowAdjacenciesSet(objType, setName);DisplayObjects();})));
+                                rows.Add(new RowProperty("Группа", new ButtonPropertyValue("Создать", () => 
+                                {
+                                    var objects = set.GetNumbers().Select(num => project.GetModelObject(objType, num)).ToList();
+                                    project.CreateGroup(set.Name, objects);
+                                    var group = project.GetAllModelGroups().Last();
+                                    console.PrintInfo(string.Format("Создана новая группа {0}", group.Name), Color.Black);
+
+                                    PresentGroupDataOnTree();
+                                    OnGroupCreated?.Invoke(group.ObjType, group.Number, group.Name);
                                 })));
                             }
-
-
                             propertiesPanel.DrawTable(rows);
                         }
  
                     }
-                    else if (objType == ObjType.Поверхность
-                        | objType == ObjType.Кривая | objType == ObjType.Точка)
+                    else if (objType == ObjType.Поверхность | objType == ObjType.Кривая | objType == ObjType.Точка)
                     {
                         //var objType = Converters.ConvertNavigatorNodeNameToObjType(arg1);
 
