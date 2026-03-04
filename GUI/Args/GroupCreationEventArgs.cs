f2990a03df0fc6bb869a1d3bc358f0e191984806
Ustinov_Nikita <unv@plm-ural.ru>
2026-03-04 17:14:59 +0500

    ADD: args for events handlers

---
+++ GUI/Args/GroupCreationEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazisGUI.Args
+{
+    public class GroupCreationEventArgs : EventArgs
+    {
+        public ObjType Type { get; }
+        public int Index { get; }
+        public string Name { get; }
+
+        public GroupCreationEventArgs(ObjType type, int index, string name)
+        {
+            Type = type;
+            Index = index;
+            Name = name;
+        }
+    }
+}
