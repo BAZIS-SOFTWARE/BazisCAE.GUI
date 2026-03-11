9cbfd73c24a7e7e3d3a22b408e51097624c18383
Ustinov_Nikita <unv@plm-ural.ru>
2026-03-11 10:48:07 +0500

    ADD: doc + example

---
+++ MasterInterface/TestMaster.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MasterInterface
+{
+    partial class TestMaster
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmbGroups = new ComboBox();
+            cmbFunctions = new ComboBox();
+            cmbMaterials = new ComboBox();
+            cmbConditions = new ComboBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // cmbGroups
+            // 
+            cmbGroups.FormattingEnabled = true;
+            cmbGroups.Location = new Point(54, 32);
+            cmbGroups.Name = "cmbGroups";
+            cmbGroups.Size = new Size(215, 23);
+            cmbGroups.TabIndex = 0;
+            // 
+            // cmbFunctions
+            // 
+            cmbFunctions.FormattingEnabled = true;
+            cmbFunctions.Location = new Point(54, 83);
+            cmbFunctions.Name = "cmbFunctions";
+            cmbFunctions.Size = new Size(215, 23);
+            cmbFunctions.TabIndex = 1;
+            // 
+            // cmbMaterials
+            // 
+            cmbMaterials.FormattingEnabled = true;
+            cmbMaterials.Location = new Point(54, 130);
+            cmbMaterials.Name = "cmbMaterials";
+            cmbMaterials.Size = new Size(215, 23);
+            cmbMaterials.TabIndex = 2;
+            // 
+            // cmbConditions
+            // 
+            cmbConditions.FormattingEnabled = true;
+            cmbConditions.Location = new Point(54, 184);
+            cmbConditions.Name = "cmbConditions";
+            cmbConditions.Size = new Size(215, 23);
+            cmbConditions.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(194, 279);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // TestMaster
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button1);
+            Controls.Add(cmbConditions);
+            Controls.Add(cmbMaterials);
+            Controls.Add(cmbFunctions);
+            Controls.Add(cmbGroups);
+            Name = "TestMaster";
+            Size = new Size(303, 340);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ComboBox cmbGroups;
+        private ComboBox cmbFunctions;
+        private ComboBox cmbMaterials;
+        private ComboBox cmbConditions;
+        private Button button1;
+    }
+}
---
+++ MasterInterface/TestMaster.cs
@@ -0,0 +1,125 @@
+﻿using MasterInterface.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterInterface
+{
+    public partial class TestMaster : BaseMaster, IGroupHandling, IFunctionsHandling, IMaterialsHandling, IPreparedDataLoader
+    {
+        public event EventHandler<EventArgs> OnFunctionsRequested;
+        public event EventHandler<EventArgs> OnMaterialsRequested;
+        public event EventHandler<EventArgs> OnGroupsRequested;
+        public event EventHandler<EventArgs> PreparedDataLoaded;
+
+        /// <summary>
+        /// Названия материалов
+        /// </summary>
+        protected List<string> materials = new List<string>();
+
+        /// <summary>
+        /// Названия функций
+        /// </summary>
+        protected List<string> functions = new List<string>();
+
+        /// <summary>
+        /// Сгруппированные по типу группы (их имена)
+        /// </summary>
+        protected Dictionary<GroupType, Dictionary<int, string>> groups = new Dictionary<GroupType, Dictionary<int, string>>();
+
+        public TestMaster() : base()
+        {
+            InitializeComponent();
+        }
+
+        public override string MasterName { get; } = "TestMaster";
+
+        public void InitialGroupFilling(Dictionary<GroupType, Dictionary<int, string>> groups)
+        {
+            this.groups.Clear();
+            foreach (var item in groups.Keys)
+            {
+                foreach (var group in groups[item])
+                {
+                    if (!this.groups.ContainsKey(item))
+                        this.groups[item] = new Dictionary<int, string>();
+
+                    this.groups[item][group.Key] = group.Value;
+                }
+            }
+
+            cmbGroups.Items.Clear();
+            cmbGroups.Items.AddRange(groups.SelectMany(x => x.Value.Values).ToArray());
+        }
+
+        public void AddGroup(GroupType type, int number, string groupName)
+        {
+            groups[type][number] = groupName;
+            cmbGroups.Items.Add(groupName);
+        }
+
+        public void RenameGroup(GroupType type, int number, string newName)
+        {
+            var oldName = groups[type][number];
+            if (cmbGroups.Text == oldName)
+                cmbGroups.Text = newName;
+
+            groups[type][number] = newName;
+            cmbGroups.Items.Remove(oldName);
+            cmbGroups.Items.Add(newName);
+        }
+
+        public void DeleteGroup(GroupType type, int number)
+        {
+            var oldName = groups[type][number];
+            if (cmbGroups.Text == oldName)
+                cmbGroups.Text = "";
+            groups[type].Remove(number);
+            cmbGroups.Items.Remove(oldName);
+        }
+
+        public void DeleteAllGroups()
+        {
+            groups.Clear();
+            cmbGroups.Items.Clear();
+            cmbGroups.Text = "";
+        }
+
+        public void SetFunctions(IEnumerable<string> functions)
+        {
+            this.functions.Clear();
+            this.functions.AddRange(functions);
+            cmbFunctions.Items.Clear();
+            cmbFunctions.Items.AddRange(functions.ToArray());
+        }
+
+        public void SetMaterials(IEnumerable<string> materials)
+        {
+            this.materials.Clear();
+            this.materials.AddRange(materials);
+            cmbMaterials.Items.Clear();
+            cmbMaterials.Items.AddRange(materials.ToArray());
+        }
+
+        public void SetDataFromConditionsStrings(IEnumerable<string> strings)
+        {
+            cmbConditions.Items.Clear();
+            cmbConditions.Items.AddRange(strings.ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var res = new List<string>();
+            foreach (var item in  cmbConditions.Items)
+                res.Add(item.ToString());
+
+            RaiseGenerateConditionsEvent(res.ToArray());
+        }
+    }
+}
