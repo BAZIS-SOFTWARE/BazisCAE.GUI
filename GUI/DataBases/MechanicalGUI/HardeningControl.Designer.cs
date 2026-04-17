bc6b8622d823bdeeb3fdf09eda2a27f114d1c800
Ustinov_Nikita <unv@plm-ural.ru>
2026-04-17 11:25:31 +0500

    ADD: localization for HardeningControl

--- GUI/DataBases/MechanicalGUI/HardeningControl.Designer.cs
+++ GUI/DataBases/MechanicalGUI/HardeningControl.Designer.cs
@@ -30,6 +30,7 @@ namespace BazisGUI.DataBases.MechanicalGUI
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HardeningControl));
             graphContainer = new GraphContainer();
             tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
@@ -47,135 +48,75 @@ namespace BazisGUI.DataBases.MechanicalGUI
             // 
             // graphContainer
             // 
-            graphContainer.Dock = System.Windows.Forms.DockStyle.Fill;
-            graphContainer.Location = new System.Drawing.Point(0, 0);
-            graphContainer.Margin = new System.Windows.Forms.Padding(0);
+            resources.ApplyResources(graphContainer, "graphContainer");
             graphContainer.Name = "graphContainer";
-            graphContainer.Size = new System.Drawing.Size(412, 488);
-            graphContainer.TabIndex = 0;
             // 
             // tableLayoutPanel1
             // 
-            tableLayoutPanel1.ColumnCount = 2;
-            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 64.69649F));
-            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35.30351F));
+            resources.ApplyResources(tableLayoutPanel1, "tableLayoutPanel1");
             tableLayoutPanel1.Controls.Add(graphContainer, 0, 0);
             tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 1, 0);
-            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 1;
-            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new System.Drawing.Size(638, 488);
-            tableLayoutPanel1.TabIndex = 1;
             // 
             // tableLayoutPanel2
             // 
-            tableLayoutPanel2.ColumnCount = 1;
-            tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            resources.ApplyResources(tableLayoutPanel2, "tableLayoutPanel2");
             tableLayoutPanel2.Controls.Add(btnCalc, 0, 1);
             tableLayoutPanel2.Controls.Add(panel1, 0, 0);
-            tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            tableLayoutPanel2.Location = new System.Drawing.Point(414, 3);
-            tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
-            tableLayoutPanel2.RowCount = 2;
-            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
-            tableLayoutPanel2.Size = new System.Drawing.Size(222, 482);
-            tableLayoutPanel2.TabIndex = 1;
             // 
             // btnCalc
             // 
-            btnCalc.Dock = System.Windows.Forms.DockStyle.Fill;
-            btnCalc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            btnCalc.Location = new System.Drawing.Point(2, 448);
-            btnCalc.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            resources.ApplyResources(btnCalc, "btnCalc");
             btnCalc.Name = "btnCalc";
-            btnCalc.Size = new System.Drawing.Size(218, 31);
-            btnCalc.TabIndex = 0;
-            btnCalc.Text = "Рассчитать";
             btnCalc.UseVisualStyleBackColor = true;
             btnCalc.Click += btnCalc_Click;
             // 
             // panel1
             // 
+            resources.ApplyResources(panel1, "panel1");
             panel1.Controls.Add(chbTemp);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
             panel1.Controls.Add(cmbPhases);
             panel1.Controls.Add(txbTemp);
-            panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            panel1.Location = new System.Drawing.Point(2, 3);
-            panel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             panel1.Name = "panel1";
-            panel1.Size = new System.Drawing.Size(218, 439);
-            panel1.TabIndex = 1;
             // 
             // chbTemp
             // 
-            chbTemp.AutoSize = true;
-            chbTemp.Location = new System.Drawing.Point(99, 59);
-            chbTemp.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            resources.ApplyResources(chbTemp, "chbTemp");
             chbTemp.Name = "chbTemp";
-            chbTemp.Size = new System.Drawing.Size(140, 19);
-            chbTemp.TabIndex = 3;
-            chbTemp.Text = "Указать температуру";
-            chbTemp.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
             chbTemp.UseVisualStyleBackColor = true;
             chbTemp.CheckedChanged += chbTemp_CheckedChanged;
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(8, 87);
-            label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            resources.ApplyResources(label2, "label2");
             label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(79, 15);
-            label2.TabIndex = 2;
-            label2.Text = "Температура";
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(26, 31);
-            label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            resources.ApplyResources(label1, "label1");
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(33, 15);
-            label1.TabIndex = 2;
-            label1.Text = "Фаза";
             // 
             // cmbPhases
             // 
-            cmbPhases.AccessibleDescription = "";
-            cmbPhases.AccessibleName = "Phases";
+            resources.ApplyResources(cmbPhases, "cmbPhases");
             cmbPhases.FormattingEnabled = true;
-            cmbPhases.Location = new System.Drawing.Point(99, 28);
-            cmbPhases.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             cmbPhases.Name = "cmbPhases";
-            cmbPhases.Size = new System.Drawing.Size(81, 23);
-            cmbPhases.TabIndex = 1;
             // 
             // txbTemp
             // 
-            txbTemp.AccessibleName = "Temp";
+            resources.ApplyResources(txbTemp, "txbTemp");
             txbTemp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            txbTemp.Enabled = false;
-            txbTemp.Location = new System.Drawing.Point(99, 83);
-            txbTemp.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             txbTemp.Name = "txbTemp";
-            txbTemp.Size = new System.Drawing.Size(81, 23);
-            txbTemp.TabIndex = 0;
             // 
             // HardeningControl
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             Controls.Add(tableLayoutPanel1);
-            Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             Name = "HardeningControl";
-            Size = new System.Drawing.Size(638, 488);
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel2.ResumeLayout(false);
             panel1.ResumeLayout(false);
