945cc90e62ef3bed6077804b600fb472b2d13cfc
agmnts <andrei.gomonec@gmail.com>
2026-04-17 12:47:12 +0500

    upd:вынесена логика визуализации геометрии в отдельный метод

--- GUI/Methods/Console/GeometryBuilder.cs
+++ GUI/Methods/Console/GeometryBuilder.cs
@@ -12,7 +12,7 @@ namespace BazisGUI
     {
         private void GeometryParser(CreateGeometryEventArgs geomCreator)
         {
-            if(GmshController == null)
+            if (GmshController == null)
             {
                 return;
             }
@@ -23,16 +23,16 @@ namespace BazisGUI
                     var prm = geomCreator.Parameters;
                     var coord = prm[0].Split(',');
 
-                    if(double.TryParse(coord[0], out double x) &&
+                    if (double.TryParse(coord[0], out double x) &&
                        double.TryParse(coord[1], out double y) &&
                        double.TryParse(coord[2], out double z))
                         AddPoint(x, y, z);
                     break;
                 case ObjType.Кривая:
                     var points = geomCreator.Parameters;
-                    if(int.TryParse(points[0], out int startTag) &&
+                    if (int.TryParse(points[0], out int startTag) &&
                        int.TryParse(points[1], out int endTag))
-                    AddLine(startTag, endTag);
+                        AddLine(startTag, endTag);
                     break;
                 case ObjType.Поверхность:
                     var lineNumbers = geomCreator.Parameters[0]
@@ -43,36 +43,37 @@ namespace BazisGUI
                     break;
                 default:
                     throw new NotSupportedException();
-            } 
+            }
             PresentGeoData();
             DisplayObjects();
         }
 
-        private void AddPoint(double x, double y, double z, double meshSize = 0) 
+        private void AddPoint(double x, double y, double z, double meshSize = 0)
         {
-            project.CreatePoint(x,y,z);
-            VBOController.DeleteVBObjects("Точка");
-            var pre = project.CreateModelObjectsPresentor(ObjType.Точка);
-            var vb = CreateVBObject(pre);
-            VBOController.AddVbo(vb);
+            project.CreatePoint(x, y, z);
+            RefreshGeometry(ObjType.Точка, "Точка");
         }
 
         private void AddLine(int startTag, int endTag, int tag = -1)
         {
             project.CreateLine(startTag, endTag);
-            VBOController.DeleteVBObjects("Кривая");
-            var pre = project.CreateModelObjectsPresentor(ObjType.Кривая);
-            var vb = CreateVBObject(pre);
-            VBOController.AddVbo(vb);
+            RefreshGeometry(ObjType.Кривая, "Кривая");
         }
 
         private void AddPlane(List<int> linesNumber)
         {
             project.CreateSurface(linesNumber.ToArray());
-            VBOController.DeleteVBObjects("Поверхность");
-            var pre = project.CreateModelObjectsPresentor(ObjType.Поверхность);
-            var vb = CreateVBObject(pre);
-            VBOController.AddVbo(vb);
+            RefreshGeometry(ObjType.Поверхность, "Поверхность");
+        }
+
+        private void RefreshGeometry(ObjType objType, string vboName)
+        {
+            VBOController.DeleteVBObjects(vboName);
+
+            var presenter = project.CreateModelObjectsPresentor(objType);
+            var vbObject = CreateVBObject(presenter);
+
+            VBOController.AddVbo(vbObject);
         }
     }
 }
