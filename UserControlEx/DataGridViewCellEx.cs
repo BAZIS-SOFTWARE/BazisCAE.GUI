5842055084d9b39b36714d25c86edfc1ab5b3a1a
Ustinov_Nikita <unv@plm-ural.ru>
2026-05-04 16:30:27 +0500

    ADD: dgvCellEx

---
+++ UserControlEx/DataGridViewCellEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControlsEx
+{
+    public partial class DataGridViewCellEx : DataGridViewCell
+    {
+        public string Key { get; set; }
+        public DataGridViewCellEx()
+        {
+            InitializeComponent();
+        }
+    }
+}
